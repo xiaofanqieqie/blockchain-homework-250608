0d4363fe7d5b5a93e35ec0f3ee9b16d032b8fba5
kimi <bbcauwbakdina@gmail.com>
2025-06-08 09:56:32 +0200

    区块链部署好了

--- opt/docker-project/eth/workspace/dapp/mine.sh
+++ opt/docker-project/eth/workspace/dapp/mine.sh
@@ -2,4 +2,4 @@
 account='1184a8e4007f05c34e8610fde3d741f1bedebace'
 mkdir -p ~/data/keystore/
 cp -r /workspace/dapp/miner/data/keystore/* ~/data/keystore/
-geth -datadir ~/data/ --networkid 88 --rpc --rpcaddr "172.26.0.50" --rpcport 8545 --rpcapi admin,eth,miner,web3,personal,net,txpool --rpcvhosts="*" --rpccorsdomain="*" --unlock ${account} --password /workspace/dapp/password.txt --etherbase ${account} console
+geth -datadir ~/data/ --networkid 88 --http --http.addr "0.0.0.0" --http.port 8545 --http.api admin,eth,miner,web3,personal,net,txpool --http.vhosts="*" --http.corsdomain="*" --allow-insecure-unlock --unlock ${account} --password /workspace/dapp/password.txt --miner.etherbase ${account} console
